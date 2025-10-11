e4e8952310d5018368b789b8bcac131cfc0e21d1
AnkineyTN <ankiney2004@gmail.com>
2025-10-12 02:17:26 +0700

    merge: resolve conflict with feature/take-notes

--- src/main/java/com/cabybara/prolearningplatform/exception/ResourceNotFoundException.java
+++ src/main/java/com/cabybara/prolearningplatform/exception/ResourceNotFoundException.java
@@ -1,8 +1,8 @@
 package com.cabybara.prolearningplatform.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 
 }
