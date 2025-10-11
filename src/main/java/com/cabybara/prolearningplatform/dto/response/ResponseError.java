19754a18c42de1a83040916116b0ee52da854dc3
luricisme <trinhnguyenluong182306@gmail.com>
2025-10-11 19:42:38 +0700

    Add response template

---
+++ src/main/java/com/cabybara/prolearningplatform/dto/response/ResponseError.java
@@ -0,0 +1,13 @@
+package com.cabybara.prolearningplatform.dto.response;
+
+import org.springframework.http.HttpStatus;
+
+public class ResponseError<T> extends ResponseData<T> {
+    public ResponseError(HttpStatus status, String message) {
+        super(status.value(), message);
+    }
+
+    public ResponseError(int status, String message) {
+        super(status, message);
+    }
+}
