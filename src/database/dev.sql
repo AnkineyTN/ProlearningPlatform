33dd67d5f82e93ac80541ea974548b91b0355f57
luricisme <trinhnguyenluong182306@gmail.com>
2025-11-02 07:47:31 +0700

    Fix something

--- src/database/dev.sql
+++ src/database/dev.sql
@@ -32,8 +32,8 @@ CREATE TABLE "user"
     recovery_code character varying(255) NULL,
     language      user_language            default 'VI',
     education     user_education           default 'HIGH_SCHOOL',
-    hear_app_from user_hear_app_from default 'GOOGLE',
-    account_type VARCHAR(50) default 'FREE',
+    hear_app_from user_hear_app_from       default 'GOOGLE',
+    account_type  VARCHAR(50)              default 'FREE',
     created_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
@@ -57,7 +57,7 @@ CREATE TABLE set
 
     CONSTRAINT fk_user
         FOREIGN KEY (id_user)
-            REFERENCES users(id)
+            REFERENCES users (id)
             ON DELETE CASCADE
 );
 
@@ -86,7 +86,7 @@ CREATE TABLE note_docs
     extension  TEXT         NOT NULL,
     public_id  VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_T  IMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_T IMESTAMP,
     id_note    INT          NOT NULL,
     CONSTRAINT fk_note
         FOREIGN KEY (id_note)
@@ -94,47 +94,49 @@ CREATE TABLE note_docs
             ON DELETE CASCADE
 );
 
-CREATE TABLE flashcard (
-                           id bigserial NOT NULL,
-                           title character varying(255) NOT NULL,
-                           description text NULL,
-                           status character varying(50) NOT NULL DEFAULT 'NOT_COMPLETED'::character varying,
-                           create_method character varying(50) NOT NULL,
-                           id_user integer NOT NULL,
-                           privacy character varying(50) NOT NULL DEFAULT 'PRIVATE'::character varying,
-                           created_at timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
-                           updated_at timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
-                           known integer NULL DEFAULT 0,
-                           learning integer NULL DEFAULT 0,
-                           remain integer NULL DEFAULT 0,
-                           id_set integer NOT NULL,
-                           last_study timestamp without time zone NULL,
+CREATE TABLE flashcard
+(
+    id            bigserial              NOT NULL,
+    title         character varying(255) NOT NULL,
+    description   text NULL,
+    status        character varying(50)  NOT NULL DEFAULT 'NOT_COMPLETED'::character varying,
+    create_method character varying(50)  NOT NULL,
+    id_user       integer                NOT NULL,
+    privacy       character varying(50)  NOT NULL DEFAULT 'PRIVATE'::character varying,
+    created_at    timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at    timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
+    known         integer NULL DEFAULT 0,
+    learning      integer NULL DEFAULT 0,
+    remain        integer NULL DEFAULT 0,
+    id_set        integer                NOT NULL,
+    last_study    timestamp without time zone NULL,
 
-                           FOREIGN KEY (id_user) REFERENCES users(id)
-                               ON DELETE CASCADE,
+    FOREIGN KEY (id_user) REFERENCES users (id)
+        ON DELETE CASCADE,
 
-                           CONSTRAINT chk_status CHECK (status IN ('COMPLETED', 'NOT_COMPLETED', 'LEARNING')),
-                           CONSTRAINT chk_privacy CHECK (privacy IN ('PUBLIC', 'PRIVATE')),
-                           CONSTRAINT chk_known_non_negative CHECK (known >= 0),
-                           CONSTRAINT chk_learning_non_negative CHECK (learning >= 0),
-                           CONSTRAINT chk_remain_non_negative CHECK (remain >= 0)
+    CONSTRAINT chk_status CHECK (status IN ('COMPLETED', 'NOT_COMPLETED', 'LEARNING')),
+    CONSTRAINT chk_privacy CHECK (privacy IN ('PUBLIC', 'PRIVATE')),
+    CONSTRAINT chk_known_non_negative CHECK (known >= 0),
+    CONSTRAINT chk_learning_non_negative CHECK (learning >= 0),
+    CONSTRAINT chk_remain_non_negative CHECK (remain >= 0)
 );
 
-CREATE TABLE card_item (
-                           id BIGSERIAL PRIMARY KEY,
-                           flashcard_id BIGINT NOT NULL,
-                           front_card TEXT NOT NULL,
-                           back_card TEXT NOT NULL,
-                           image_url VARCHAR(255),
+CREATE TABLE card_item
+(
+    id           BIGSERIAL PRIMARY KEY,
+    flashcard_id BIGINT      NOT NULL,
+    front_card   TEXT        NOT NULL,
+    back_card    TEXT        NOT NULL,
+    image_url    VARCHAR(255),
 
-                           card_status VARCHAR(50) NOT NULL DEFAULT 'NEW',
+    card_status  VARCHAR(50) NOT NULL DEFAULT 'NEW',
 
-                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                           updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at   TIMESTAMP            DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP            DEFAULT CURRENT_TIMESTAMP,
 
-                           FOREIGN KEY (flashcard_id) REFERENCES flashcard(id) ON DELETE CASCADE,
+    FOREIGN KEY (flashcard_id) REFERENCES flashcard (id) ON DELETE CASCADE,
 
-                           CONSTRAINT chk_card_status CHECK (card_status IN ('NEW', 'LEARNING', 'KNOWN'))
+    CONSTRAINT chk_card_status CHECK (card_status IN ('NEW', 'LEARNING', 'KNOWN'))
 );
 
 
