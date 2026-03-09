2c42c8dd22673fa8a6a4c1d968be4bdf7f2a0ed2
AnkineyTN <ankiney2004@gmail.com>
2026-03-09 10:28:43 +0700

    chore: add validation for exam request dtos

--- src/main/java/com/cabybara/prolearningplatform/dto/request/exam/CreateQuestionRequestDto.java
+++ src/main/java/com/cabybara/prolearningplatform/dto/request/exam/CreateQuestionRequestDto.java
@@ -1,14 +1,22 @@
 package com.cabybara.prolearningplatform.dto.request.exam;
 
 import com.cabybara.prolearningplatform.enums.QuestionType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record CreateQuestionRequestDto(
+        @NotNull(message = "Content cannot be null")
         String content,
-        QuestionType type,
+
+        @NotNull(message = "Question type cannot be null")
+        String type,
+
+        @Min(value = 1, message = "Points must be at least 1")
         Integer point,
+
         List<QuestionOptionDto> options
 ) {}
--- src/main/java/com/cabybara/prolearningplatform/dto/request/exam/CreateQuizRequestDto.java
+++ src/main/java/com/cabybara/prolearningplatform/dto/request/exam/CreateQuizRequestDto.java
@@ -1,10 +1,12 @@
 package com.cabybara.prolearningplatform.dto.request.exam;
 
 import com.cabybara.prolearningplatform.enums.Privacy;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CreateQuizRequestDto(
+        @NotNull(message = "Title cannot be null")
         String title,
         Privacy privacy,
         String description
