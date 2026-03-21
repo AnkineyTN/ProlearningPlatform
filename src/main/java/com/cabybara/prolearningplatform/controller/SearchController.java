8ff2f4b5e36ad4808b026f4726f68979fc9b5f5a
AnkineyTN <ankiney2004@gmail.com>
2026-03-21 17:22:23 +0700

    chore: add pagination for search api

--- src/main/java/com/cabybara/prolearningplatform/controller/SearchController.java
+++ src/main/java/com/cabybara/prolearningplatform/controller/SearchController.java
@@ -1,12 +1,17 @@
 package com.cabybara.prolearningplatform.controller;
 
+import com.cabybara.prolearningplatform.dto.response.PaginationResponseDto;
 import com.cabybara.prolearningplatform.dto.response.search.SearchResponseDto;
 import com.cabybara.prolearningplatform.service.search.SearchService;
 import com.cabybara.prolearningplatform.utils.ApiResponse;
 import com.cabybara.prolearningplatform.utils.ResponseUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -27,11 +32,36 @@ public class SearchController {
     private final SearchService searchService;
 
     @GetMapping()
-    public ResponseEntity<ApiResponse<?>> searchAllForUser(@RequestParam String keyword, @RequestParam int limit) {
+    @Operation(
+            summary = "Search all resource (note, set, flashcard, exam) belongs to the current user"
+    )
+    public ResponseEntity<ApiResponse<?>> searchAllForUser(
+            @RequestParam String keyword,
+            @RequestParam int limit,
+            @ParameterObject @PageableDefault(page = 0, size = 6, sort = "id") Pageable pageable
+    ) {
         List<SearchResponseDto> searchResponseDtos = searchService.searchForUser(keyword, limit);
+
+        int totalItems = searchResponseDtos.size();
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+
+        int start = currentPage * pageSize;
+        int end = Math.min(start + pageSize, totalItems);
+
+        List<SearchResponseDto> pagedResponse =
+                start >= totalItems ? List.of() : searchResponseDtos.subList(start, end);
+
+        int totalPages = (int) Math.ceil((double) totalItems / pageSize);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(ResponseUtil.success("Successfully", searchResponseDtos, null));
+                .body(ResponseUtil.success("Successfully", pagedResponse, PaginationResponseDto.builder()
+                        .pageSize(pageSize)
+                        .currentPage(currentPage)
+                        .totalItems(totalItems)
+                        .totalPages(totalPages)
+                        .build()
+                ));
     }
 }
