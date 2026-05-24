b2558f47080a996f70dc726ffb47b53d35d38b04
AnkineyTN <ankiney2004@gmail.com>
2026-05-24 16:38:22 +0700

    feat: add user-specific caching for flashcard, exam, and note detail endpoints

--- src/main/java/com/cabybara/prolearningplatform/dto/response/note/GetDocsInNoteResponseDTO.java
+++ src/main/java/com/cabybara/prolearningplatform/dto/response/note/GetDocsInNoteResponseDTO.java
@@ -3,10 +3,12 @@ package com.cabybara.prolearningplatform.dto.response.note;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class GetDocsInNoteResponseDTO {
